4a93044322374dcd9c34ad15dab0665e897b08cd
alebo962 <alebo62@inbox.ru>
2026-01-29 20:33:39 +0200

    message()

---
+++ Ep011/message.cmake
@@ -0,0 +1,13 @@
+# message("The sky is blue")
+# message([=[The
+#  sky 
+#   is 
+#    blue]=])
+
+# message("Sky is blue \" my \n friend")
+# message("Multi 
+#          .... lines ${CMAKE_VERSION}")
+
+message(the\ sky\ is blue)
+message(Two arguments)
+message(The;sky;is;blue)
