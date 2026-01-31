edc0011c28073dcc48f7c501c88de32e322f3bbf
alebo62 <alebo62@inbox.ru>
2026-01-31 17:40:31 +0300

    add variables: set(...)

--- Ep011/message.cmake
+++ Ep011/message.cmake
@@ -11,3 +11,15 @@
 message(the\ sky\ is blue)
 message(Two arguments)
 message(The;sky;is;blue)
+
+# Variables
+
+set(var1 "Text") # set([[var1]] "Text") set("var1" "Text") 
+message(${var1})
+set(ENV{MY_PATH} "/home/alex")
+message($ENV{MY_PATH})
+message($ENV{PATH})
+# cache vars
+set(cache_var "CV" CACHE STRING "CS Value")
+message(${cache_var})
+message(${CMAKE_VERSION} ${CMAKE_COMMAND} ${CMAKE_CURRENT_LIST_FILE})
